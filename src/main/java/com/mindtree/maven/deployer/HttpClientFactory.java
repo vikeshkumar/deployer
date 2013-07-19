4eff9e5bb6bcb35060945eedd61a1508d24268e1
Vikesh Kumar <vikesh.kumar2@gmail.com>
2013-07-20 03:26:26 +0530

    Working on no proxy, have to test on proxy

---
+++ src/main/java/com/mindtree/maven/deployer/HttpClientFactory.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mindtree.maven.deployer;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+ *
+ * @author Vikesh
+ */
+public class HttpClientFactory {
+
+    public static DefaultHttpClient getHttpClient() {
+        return null;
+    }
+}
